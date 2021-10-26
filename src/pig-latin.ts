b6bedaae737539e47b2f23142aade23e3015cd51
Ben Wainwright <ben.wainwright@cinch.co.uk>
2021-10-26 10:12:05 +0100

    add comments

--- src/pig-latin.ts
+++ src/pig-latin.ts
@@ -1,3 +1,11 @@
+
+/* 
+ * Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
+ *
+ *
+ * pigIt('Pig latin is cool') --> igPay atinlay siay oolcay
+ * pigIt('Hello world !') --> elloHay orldway !
+ */
 export const  pigIt = (text: string) => {
   return ""
 }
