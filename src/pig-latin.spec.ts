62f4e7b712779da62a9034193bcf7b399f97715d
Ben Wainwright <ben.wainwright@cinch.co.uk>
2021-10-26 10:10:16 +0100

    initial commit

---
+++ src/pig-latin.spec.ts
@@ -0,0 +1,6 @@
+import { pigIt } from "./pig-latin"
+
+test("a test", () => {
+  const result = pigIt("something")
+  expect(true).toEqual(false)
+})
---
+++ src/pig-latin.ts
@@ -0,0 +1,3 @@
+export const  pigIt = (text: string) => {
+  return ""
+}
